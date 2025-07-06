ecb6dd78694be4a4551e335533f43a5c5923104a
yanis21332 <kerroucheker@gmail.com>
2025-07-06 22:03:43 +0200

    reglalge du probleme music

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -395,7 +395,7 @@ const MenuPage = () => {
   return (
     <>
       <audio ref={audioRef} loop>
-        <source src={"https://o.uguu.se/EuBvepen.mp3"} type="audio/mp3" />
+        <source src={"http://158.220.93.227/music/resto_music.mp3"} type="audio/mp3" />
         Your browser do not support this format.
       </audio>
       {!isPlaying && (
