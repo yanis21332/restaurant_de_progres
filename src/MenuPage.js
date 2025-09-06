832e76531f953e21a1f9f327c3145d6206438513
yanis21332 <kerroucheker@gmail.com>
2025-09-06 11:30:03 +0400

    ....

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -374,7 +374,7 @@ const MenuPage = () => {
   return (
     <>
       <audio ref={audioRef} loop>
-        <source src={"https://shockzone.online/music.mp3"} type="audio/mp3" />
+        <source src={"/music_light.mp3"} type="audio/mp3" />
         Your browser do not support this format.
       </audio>
 
