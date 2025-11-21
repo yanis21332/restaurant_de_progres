eb3c025fcdc4578d214f109ae33d5eba239c869d
yanis21332 <66674136+yanis21332@users.noreply.github.com>
2025-11-21 21:22:49 +0400

    Update MenuPage.js

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -515,7 +515,7 @@ const MenuPage = () => {
                         return (
                           <div key={i} className="oneDish">
                             <h3>
-                              {dishe.name} {dishe.name.toLowerCase() === "tiramisu" ? "(250DA)" : "س"}
+                              {dishe.name} {dishe.name.toLowerCase() === "tiramisu" ? "(250DA)" : "${dishe.name.toLowerCase()}"}
                             </h3>
                           </div>
                         );
